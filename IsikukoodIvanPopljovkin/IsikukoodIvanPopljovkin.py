dae25fbff1b51460e236d2ddaa7674e631d4fe5c
jovkin27 <118188660+jovkin27@users.noreply.github.com>
2023-01-06 12:56:13 +0200

    Добавьте файлы проекта.

---
+++ IsikukoodIvanPopljovkin/IsikukoodIvanPopljovkin.py
@@ -0,0 +1,35 @@
+﻿from datetime import datetime
+from модуль1 import*
+while True:
+    ik=input("Isikukood: ")
+    if len(ik)==11:
+        try:
+            ik_list=list(ik) #ik_list["4","9","9"...,...]
+            if int(ik_list[0]) in [1,2,3,4,5,6]:
+                if int(ik_list[0]) in [1,2]:
+                    a=1800
+                elif int(ik_list[0]) in [3,4]:
+                    a=1900
+                else:
+                    a=2000
+                aasta=a+int(ik_list[1]+ik_list[2])
+                kuu=int(ik_list[3]+ik_list[4])
+                paev=int(ik_list[5]+ik_list[6])
+                if datetime(aasta,kuu,paev):
+                    vastus=Kontroll(ik)   #                 
+                    if type(vastus)==int:
+                        ik3=int(ik[7:10])
+                        haigla=Haigla(ik3)
+                        print(haigla)
+                        sugu=Sugu(int(ik_list[0]))
+                        print(sugu)
+                    else:
+                        print(vastus) #
+
+            else:
+                print("Esimene sümbol/arv on vale ")
+        except:
+            print("Andmetüüp on vale, On vaja numbreid sisestada")
+    else:
+        print("Sümbolite arv peab 11 olema ")
+#50304273717
---
+++ IsikukoodIvanPopljovkin/модуль1.py
@@ -0,0 +1,62 @@
+﻿def Kontroll(isikukood:str):
+    """Isikukoodi kontroll number
+    On vaja isikukood sisestada
+    :param str ik: Inimese isikukood
+    :rtype: var Märamata tüüp
+    """
+    ik_list=list(isikukood)
+    s=0
+    for i in range(0,10):
+        s+=(i%9+1)*int(ik_list[i])
+        print(f"{i%9+1}*{int(ik_list[i])}+", end="")
+    print(s)
+    s=s-(s//11)*11
+    print(s)
+    if s==int(ik_list[10]):
+        vastus=s
+    else:
+        vastus="Kontroll number on vale"
+    return vastus
+def Haigla(ik3:int):
+    """Haigla 3 isikukoodi numbri alusel
+    :param int ik3:
+    :rtype: str Haigla ja koht
+    """
+    if ik3 in range(1,11):
+        haigla='Kuresaare'
+    elif ik3 in range(11,20):
+        haigla='Tartu'
+    elif ik3 in range(21,221):
+        haigla='Ida-Tallinna Keskhaigla, Pelgulinna sünnitusmaja, Hiiumaa, Keila, Rapla haigla, Loksa haigla'
+    elif ik3 in range(221,271):
+        haigla=' Ida-Viru Keskhaigla (Kohtla-Järve, endine Jõhvi)'
+    elif ik3 in range(271,371):
+        haigla=' Maarjamõisa Kliinikum (Tartu), Jõgeva Haigla'
+    elif ik3 in range(371,421):
+        haigla='Narva Haigla'
+    elif ik3 in range(421,471):
+        haigla='Pärnu Haigla'
+    elif ik3 in range(471,491):
+        haigla='Pelgulinna Sünnitusmaja (Tallinn), Haapsalu haigla'
+    elif ik3 in range(491,520):
+        haigla='Järvamaa Haigla (Paide)' 
+    elif ik3 in range(521,571):
+        haigla='Rakvere, Tapa haigla'
+    elif ik3 in range(571,601):
+        haigla='Valga Haigla'
+    elif ik3 in range(601,651):
+        haigla='Viljandi Haigla'
+    else:
+        haigla='Viljandi Haigla' 
+    return haigla
+def Sugu(ik1:int)->str:
+    """
+    Sugu 1 number
+    :param int ik1
+    :rtype: str Sugu
+    """
+    if ik1%2==0:
+        sugu="naine"
+    else:
+        sugu="mees"
+    return sugu
